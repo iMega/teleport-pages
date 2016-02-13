049205c8389e6cf77e1cdff4628aeabccbd8112f
Dmitry <irvis@imega.ru>
2016-02-13 20:22:30 +0300

    Add activate

---
+++ js/common.js
@@ -0,0 +1,20 @@
+var teleportApp = angular.module('teleportApp', ['ngRoute'], function ($interpolateProvider) {
+    $interpolateProvider.startSymbol('{|').endSymbol('|}');
+});
+
+teleportApp.config(function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+});
+
+teleportApp.controller('signupCtrl', ['$http', '$location', function ($http, $location) {
+    this.sendInvite = function () {
+        $http.get('http://a.imega.club/activate/invite/' + this.email);
+    };
+
+    this.activate = function () {
+        if ($location.search().hasOwnProperty('activate')) {
+            var token = $location.search()['activate'];
+            $http.get('http://a.imega.club/activate/account/' + token);
+        }
+    }
+}]);
